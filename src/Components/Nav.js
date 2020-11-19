8ad4a8584b5f9f3d82e1b9da7bd5bb0b8e7d9ca4
pfalzergbr <64397919+pfalzergbr@users.noreply.github.com>
2020-11-19 16:00:34 +0000

    start styling, add variables

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,35 +1,43 @@
 import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
-import Modal from 'react-modal'
+import Modal from 'react-modal';
 
 import { AuthContext } from '../Context/MainContext';
-import NewTrade from '../Pages/NewTrade'
+import NewTrade from '../Pages/NewTrade';
 
 const Nav = (props) => {
     const links = props.data;
     const { token, logout } = useContext(AuthContext);
-    const [ modalIsOpen, setModalIsOpen ] = useState(false);
-    
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
     const openModal = () => {
         setModalIsOpen(true);
-    }
+    };
 
     const closeModal = () => {
         setModalIsOpen(false);
-    }
+    };
 
     return (
-        <div>
-            <Modal isOpen={modalIsOpen} onRequestClose={closeModal}><NewTrade closeModal={closeModal} /></Modal>
-            {links.map((link) => (
-                <Link key={link.to} exact to={link.to}>
-                    {link.name}
-                </Link>
+        <div className='main-navigation container'>
+            <div className='main-navigation__navlinks'>
+                <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
+                    <NewTrade closeModal={closeModal} />
+                </Modal>
+                {links.map((link) => (
+                    <Link key={link.to} exact to={link.to}>
+                        {link.name}
+                    </Link>
                 ))}
-            {token && <span>Welcome, {props.user && props.user.userName}</span>}
-            {/* Logout button for the time being*/}
-            {token && <button onClick={logout}>Logout</button>}
-            {token && <button onClick={openModal}>QuickTrade</button>}
+            </div>
+            <div className='main-navigation__user-container'>
+                {token && (
+                    <span>Welcome, {props.user && props.user.userName}</span>
+                )}
+                {/* Logout button for the time being*/}
+                {token && <button onClick={logout}>Logout</button>}
+                {token && <button onClick={openModal}>QuickTrade</button>}
+            </div>
         </div>
     );
 };
