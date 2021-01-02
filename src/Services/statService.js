35e934666a25efd36774f0b04018d7bb215ffda5
pfalzergbr <64397919+pfalzergbr@users.noreply.github.com>
2021-01-02 10:49:41 +0000

    add fix digits to win percentage

--- src/Services/statService.js
+++ src/Services/statService.js
@@ -1,4 +1,4 @@
 export const calcWinPercentage = ( numOfWins, numOfLoss ) => {
     const totalTrades = parseFloat(numOfWins) + parseFloat(numOfLoss);
-    return (parseFloat(numOfWins) / totalTrades) * 100;
+    return ((parseFloat(numOfWins) / totalTrades) * 100).toFixed(2);
 }
