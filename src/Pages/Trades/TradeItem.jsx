47689b6a05efbb6c01f99c41da0eca18b73a8051
pfalzergbr <64397919+pfalzergbr@users.noreply.github.com>
2021-01-08 18:26:38 +0000

    start working on trade list

--- src/Pages/Trades/TradeItem.jsx
+++ src/Pages/Trades/TradeItem.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 const TradeItem = props => {
   // const { _id, symbol, outcome, amount, date, description, strategy, accountId, userId, createdAt} = props.data
-  return <div>{props.data.symbol}</div>;
+  return (
+    <div>
+      <span>{props.data.symbol}</span>
+      <span>{props.data.amount}</span>
+    </div>
+  );
 };
 
 export default TradeItem;
