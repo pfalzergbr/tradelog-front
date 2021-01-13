2f9b2efc8a92c0f404de0f0fb292c6526267ddfc
pfalzergbr <64397919+pfalzergbr@users.noreply.github.com>
2021-01-13 16:22:19 +0000

    add footer to the landing page

--- src/Pages/Shared/Footer.jsx
+++ src/Pages/Shared/Footer.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='footer container'>
+    <footer className='footer'>
       <p className='footer__disclaimer'>
-        <span className='footer__disclaimer--tag'>Disclaimer: </span>
+        <span className='footer__disclaimer__tag'>Disclaimer: </span>
         This is a practice project made for Full Stack web development
         portfolio, does not provide invesment advice in any way. Please only
         invest at your own risk. The creator of this site does not take any
-        responsibility for your trading losses in any way.{' '}
+        responsibility for your trading losses in any way.
       </p>
       <p className='footer__trademark'>TradeLog 2021</p>
     </footer>
