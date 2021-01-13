398b3cd46d11185a1ffefccf7bd112d79a9f82a3
pfalzergbr <64397919+pfalzergbr@users.noreply.github.com>
2021-01-13 16:12:59 +0000

    add svgs

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -4,19 +4,30 @@ import Footer from '../Shared/Footer';
 
 const Landing = () => {
   return (
-    <div>
-      <div className='landing-page'>
+    <div className='landing-page'>
+      <div className='landing-page__container'>
         <main className='landing-page__main'>
-          <h1>Keep track of your trades. Find your edge. Grow your profits</h1>
-          <h3>
+          <h1 className='landing-page__title'>Keep track of your trades. Find your edge. Grow your profits</h1>
+          <h3 className='landing-page__subtitle'>
             Analyse your accounts. Find your most profitable trading strategies.
             Repeat.
           </h3>
           <button className='btn btn--primary'>Sign up for free</button>
         </main>
-        <div className='landing-page__graphic-container'></div>
-        <Footer />;
+        <div className='landing-page__graphic-column'>
+          <div className='graphic'>
+            <object
+              className='graphic--background'
+              type='image/svg+xml'
+              data='./svg/icon-group.svg'></object>
+            <object
+              className='graphic--front'
+              type='image/svg+xml'
+              data='./svg/icon-group2.svg'></object>
+          </div>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 };
