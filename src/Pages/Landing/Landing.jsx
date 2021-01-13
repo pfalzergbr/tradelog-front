3c9f3a1c0b8a6473fdf3244f0e796338bc39cbb0
pfalzergbr <64397919+pfalzergbr@users.noreply.github.com>
2021-01-13 12:25:25 +0000

    start styling landing

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -4,14 +4,21 @@ import Footer from '../Shared/Footer';
 
 const Landing = () => {
   return (
-    <div className='landing-page'>
-      <main className='landing-page__main'>
-      </main>
-      <Footer />;
+    <div>
+      <div className='landing-page'>
+        <main className='landing-page__main'>
+          <h1>Keep track of your trades. Find your edge. Grow your profits</h1>
+          <h3>
+            Analyse your accounts. Find your most profitable trading strategies.
+            Repeat.
+          </h3>
+          <button className='btn btn--primary'>Sign up for free</button>
+        </main>
+        <div className='landing-page__graphic-container'></div>
+        <Footer />;
+      </div>
     </div>
   );
 };
-
-
 
 export default Landing;
