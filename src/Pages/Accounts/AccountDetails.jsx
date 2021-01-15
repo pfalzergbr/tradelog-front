db0535d58907624066d26be8a0db353880e68351
pfalzergbr <64397919+pfalzergbr@users.noreply.github.com>
2021-01-15 10:16:49 +0000

    display only account related strategies in accordion

--- src/Pages/Accounts/AccountDetails.jsx
+++ src/Pages/Accounts/AccountDetails.jsx
@@ -6,6 +6,7 @@ import {
   selectAccount,
   selectAccountStats,
 } from '../../Redux/Reducers/account';
+import { selectAccountStrategies } from '../../Redux/Reducers/strategy';
 
 import { loadStrategyStats } from '../../Services/Requests/strategyRequests';
 import { loadAccountStats } from '../../Services/Requests/accountRequests';
@@ -19,15 +20,15 @@ import AccountDetailsHeader from './AccountDetailsHeader';
 const AccountDetails = () => {
   const { accountId } = useParams();
   const { token } = useSelector(state => state.auth);
-  const { strategies } = useSelector(state => state.strategy);
   const { trades } = useSelector(state => state.trade);
   const account = useSelector(state => selectAccount(state, accountId)) || {};
   const accountStats =
     useSelector(state => selectAccountStats(state, accountId)) || {};
   const dispatch = useDispatch();
+  const accountStrategies =
+    useSelector(state => selectAccountStrategies(state, accountId)) || {};
 
   useEffect(() => {
-    //Add selectors to check if it is already fetched.
     loadAccountStats(token, dispatch);
   }, [dispatch, token]);
 
@@ -46,10 +47,13 @@ const AccountDetails = () => {
   return (
     <LoadingGroup>
       <div>
-        <AccountDetailsHeader account={account} token={token}/>
+        <AccountDetailsHeader account={account} token={token} />
         <div className='account-details'>
           <div className='strategies-column'>
-            <AccordionMenu account={accountStats} strategies={strategies} />
+            <AccordionMenu
+              account={accountStats}
+              strategies={accountStrategies}
+            />
           </div>
           <div className='trades-column'>
             <TradeList trades={trades} />
