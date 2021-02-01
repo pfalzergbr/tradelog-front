650935e8b06fcdafc219f2cde1e57f959673e393
pfalzergbr <64397919+pfalzergbr@users.noreply.github.com>
2021-02-01 07:28:45 +0000

    add basic styles to mobile version of account details

--- src/Pages/Strategies/SmallScreen/StrategyWidget.jsx
+++ src/Pages/Strategies/SmallScreen/StrategyWidget.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../../Shared/ui/Button';
 
 const StrategyWidget = () => {
   return (
@@ -6,6 +7,9 @@ const StrategyWidget = () => {
       <div className='widget__container'>
         <h4 className='widget__title'>Account - AccountName</h4>
         <p className='widget__value'>500$</p>
+      </div>
+      <div className="widget__button-container">
+        <Button buttonStyle='outline'>Details</Button>
       </div>
     </div>
   );
