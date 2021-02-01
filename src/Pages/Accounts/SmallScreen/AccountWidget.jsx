697a4fe5fa34d6503f500bb0b0f7316859f5b439
pfalzergbr <64397919+pfalzergbr@users.noreply.github.com>
2021-02-01 08:28:37 +0000

    try new layout for account details

--- src/Pages/Accounts/SmallScreen/AccountWidget.jsx
+++ src/Pages/Accounts/SmallScreen/AccountWidget.jsx
@@ -8,8 +8,14 @@ const AccountWidget = ({ account }) => {
         <h4 className='widget__title'>Account - AccountName</h4>
         <p className='widget__value'>500$</p>
       </div>
+      <div className='widget__details-container'>
+        <p className="widget__details">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis omnis ad molestias repudiandae eligendi! Autem aspernatur tenetur magnam ut recusandae doloribus maiores dolorem nesciunt officiis ab? Ducimus cum neque vero.</p>
+      </div>
       <div className='widget__button-container'>
+        <Button>Edit</Button>
+        <Button>Delete</Button>
         <Button buttonStyle='outline'>Details</Button>
+        <Button buttonStyle='outline'>New Strategy</Button>
       </div>
     </div>
   );
