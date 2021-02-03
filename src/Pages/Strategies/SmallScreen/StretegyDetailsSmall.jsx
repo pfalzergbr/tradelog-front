6124e6a1305f1c14f33872a45fb0b3bf16225b38
pfalzergbr <64397919+pfalzergbr@users.noreply.github.com>
2021-02-03 09:06:51 +0000

    add data to StrategyDetailsSmall

--- src/Pages/Strategies/SmallScreen/StretegyDetailsSmall.jsx
+++ src/Pages/Strategies/SmallScreen/StretegyDetailsSmall.jsx
@@ -2,7 +2,10 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { selectAccountCurrency } from '../../../Redux/Reducers/account';
-import { selectStrategyStat, selectStrategy } from '../../../Redux/Reducers/strategy';
+import {
+  selectStrategyStat,
+  selectStrategy,
+} from '../../../Redux/Reducers/strategy';
 import { loadStrategyStats } from '../../../Services/Requests/strategyRequests';
 
 import Card from '../../Accounts/AccountCard/Card';
@@ -13,14 +16,17 @@ const StrategyDetailsSmall = () => {
   const dispatch = useDispatch();
   const { strategyId } = useParams();
   const { token } = useSelector(state => state.auth);
-  const strategy = useSelector(state => selectStrategy(state, strategyId));
+  const strategy =
+    useSelector(state => selectStrategy(state, strategyId)) || {};
   const strategyStats = useSelector(state =>
     selectStrategyStat(state, strategyId),
   );
-  const currency = useSelector(state => selectAccountCurrency(state, strategy.account_id)) || ''
-  
+  const currency =
+    useSelector(state => selectAccountCurrency(state, strategy.account_id)) ||
+    '';
+
   useEffect(() => {
-    if (strategy && ! strategyStats) {
+    if (strategy && !strategyStats) {
       loadStrategyStats(token, strategy, dispatch);
     }
   }, [token, strategy, strategyStats, dispatch]);
@@ -28,7 +34,7 @@ const StrategyDetailsSmall = () => {
   return (
     <LoadingGroup>
       {strategy && strategyStats && (
-        <Card>
+        <Card type='strategy-details'>
           <StatContainer
             text={strategy.strategy_name}
             value={strategyStats.total_pnl}
@@ -36,6 +42,51 @@ const StrategyDetailsSmall = () => {
             variant='accordion-heading'
             containerClass='item-container'
             currency={currency}
+          />
+
+          <StatContainer
+            text='Win Percentage'
+            value={strategyStats.winPercentage}
+            type='percentage'
+            variant='accordion-content'
+            containerClass='item-container'
+          />
+          <StatContainer
+            text='Average Profit'
+            value={strategyStats.average_profit}
+            type='amount'
+            variant='accordion-content'
+            containerClass='item-container'
+            currency={currency}
+          />
+          <StatContainer
+            text='Average Loss'
+            value={strategyStats.average_loss}
+            type='amount'
+            variant='accordion-content'
+            containerClass='item-container'
+            currency={currency}
+          />
+          <StatContainer
+            text='Number of Trades'
+            value={strategyStats.num_of_trades}
+            type='value'
+            variant='accordion-content'
+            containerClass='item-container'
+          />
+          <StatContainer
+            text='Wins'
+            value={strategyStats.num_of_profit}
+            type='value'
+            variant='accordion-content'
+            containerClass='item-container'
+          />
+          <StatContainer
+            text='Losses'
+            value={strategyStats.num_of_loss}
+            type='value'
+            variant='accordion-content'
+            containerClass='item-container'
           />
         </Card>
       )}
